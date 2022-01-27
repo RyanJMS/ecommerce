a6f43ec1474c9feefdec62ae85b96a31b05881b5
Ryan <jukkasc2@gmail.com>
2022-01-27 17:42:39 -0500

    Rendered real products from Commerce.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,26 +4,7 @@ import Product from "./Product/Product";
 
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running Shoes",
-    price: "$40",
-    image:
-      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQppRsMF_kMTm6CuvbAU_dbbqI4Fkwc_TmDU8LjO3PUE8eI4gESBL07kfdFCm5l9lMgdO8-TAnrjJmJjMb2MlfLG0vwf-TL9N3eDnTbESgiU8BiJkXx1_OnTg&usqp=CAE",
-  },
-  {
-    id: 2,
-    name: "Macbook>",
-    description: "Apple Macbook",
-    price: "$500",
-    image:
-      "https://www.thesource.ca/medias/20201112120946-108092084-A.jpg-mediaConversion-640-x-480-mediaConversion-400-x-300-0?context=bWFzdGVyfGltYWdlc3w0MjQzOHxpbWFnZS9qcGVnfGltYWdlcy9oYjAvaDJlLzkzMTkwMTgxMDI4MTQuanBnfDNlZjAxNDZlODA3NDlmNTVhYTdjNDAzMTU5ZGI2OWMwNzQyYTRhYjg3YTQ4MjFjYWZlM2Y5OTAzMjNmNmU4Mzg",
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
